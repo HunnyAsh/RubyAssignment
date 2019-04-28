b8fca5448eb04aa9683e1d72a15384373b60728b
HunnyAsh <jummanihunny@gmail.com>
2019-04-28 13:54:12 +0530

    Diamond Pattern For chars

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -1,17 +1,21 @@
 class Diamond
   def self.make_diamond(char)
     no_of_rows = char.ord - 65 + 1
-    (1..no_of_rows).each do |i|
-      (1..no_of_rows).each do |j|
-        if j + i == no_of_rows / 2 || i + j == ((no_of_rows - 1) + no_of_rows / 2) || j == i + no_of_rows / 2 || i == j + no_of_rows / 2
-          print char
-        else
-          print ' '
-        end
-      end
-      print "\n"
-    end
+    start_char = 'A'
+      no_of_rows.times {|i|
+        print ' ' * (no_of_rows - i)
+        print "#{start_char} " * (i + 1)
+        print "\n"
+        start_char = start_char.next
+      }
+      start_char = (start_char.ord - 2).chr
+      (no_of_rows-2).downto(0) {|i|
+        print ' ' * (no_of_rows - i)
+        print "#{start_char} " * (i + 1)
+        print "\n"
+        start_char = (start_char.ord - 1).chr
+      }
   end
 end
 
-Diamond.make_diamond('U')
+Diamond.make_diamond('E')
