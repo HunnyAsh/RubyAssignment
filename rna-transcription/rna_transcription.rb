7a43ecd71830c5fdb4649e3d7911f19537c81ce1
HunnyJummani <hunny.jummani@botreetechnologies.com>
2019-09-22 08:02:36 +0530

    RNA changes

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -3,7 +3,7 @@ class Complement
   DNA = ['G', 'C', 'T', 'A'].freeze
   RNA = ['C', 'G', 'A', 'U'].freeze
   def self.of_dna(input)
-    input_chars = input.chars.map { |dna_char| RNA[DNA.index(dna_char) ? DNA.index(dna_char) : dna_char]}
+    input_chars = input.chars.map { |dna_char| RNA[DNA.index(dna_char)]}
     input_chars.join('')
   end
 end
